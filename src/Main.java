ba55fd2fa6f5316b71221f01a31974382cabdc54
TenetNoctis <noctis.eden@gmail.com>
2025-05-24 17:57:26 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class StudentManager {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainMenu().setVisible(true);
+            }
+        });
+    }
+}
